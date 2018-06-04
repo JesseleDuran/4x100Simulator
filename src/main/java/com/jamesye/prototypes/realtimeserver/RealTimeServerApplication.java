4bf37c227ce3195a67f9dd101021762c0e11ac36
Jessele Duran <jesseleadt@gmail.com>
2018-06-04 19:40:29 -0400

    fix: no DB connection

--- src/main/java/com/jamesye/prototypes/realtimeserver/RealTimeServerApplication.java
+++ src/main/java/com/jamesye/prototypes/realtimeserver/RealTimeServerApplication.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.hibernate.SessionFactory;
+import org.hibernate.Session;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 @SpringBootApplication
 public class RealTimeServerApplication {
@@ -16,6 +20,9 @@ public class RealTimeServerApplication {
     @Value("${rt.server.port}")
     private Integer port;
 
+    static Session sessionObj;
+    static SessionFactory sessionFactoryObj;
+
     @Bean
     public SocketIOServer socketIOServer() {
         Configuration config = new Configuration();
@@ -24,7 +31,50 @@ public class RealTimeServerApplication {
         return new SocketIOServer(config);
     }
 
+    private static SessionFactory buildSessionFactory() {
+        // Creating Configuration Instance & Passing Hibernate Configuration File
+        org.hibernate.cfg.Configuration configObj = new org.hibernate.cfg.Configuration();
+        configObj.configure("hibernate.cfg.xml");
+
+        // Since Hibernate Version 4.x, ServiceRegistry Is Being Used
+        ServiceRegistry serviceRegistryObj = new StandardServiceRegistryBuilder().applySettings(configObj.getProperties()).build();
+
+        // Creating Hibernate SessionFactory Instance
+        sessionFactoryObj = configObj.buildSessionFactory(serviceRegistryObj);
+        return sessionFactoryObj;
+    }
+
     public static void main(String[] args) {
+        /*System.out.println(".......Hibernate Maven Example.......\n");
+        try {
+            sessionObj = buildSessionFactory().openSession();
+            sessionObj.beginTransaction();
+
+            for(int i = 101; i <= 105; i++) {
+                userObj = new User();
+                userObj.setUserid(i);
+                userObj.setUsername("Editor " + i);
+                userObj.setCreatedBy("Administrator");
+                userObj.setCreatedDate(new Date());
+
+                sessionObj.save(userObj);
+            }
+            System.out.println("\n.......Records Saved Successfully To The Database.......\n");
+
+            // Committing The Transactions To The Database
+            sessionObj.getTransaction().commit();
+        } catch(Exception sqlException) {
+            if(null != sessionObj.getTransaction()) {
+                System.out.println("\n.......Transaction Is Being Rolled Back.......");
+                sessionObj.getTransaction().rollback();
+            }
+            sqlException.printStackTrace();
+        } finally {
+            if(sessionObj != null) {
+                sessionObj.close();
+            }
+        }*/
+
         SpringApplication.run(RealTimeServerApplication.class, args);
     }
 }
