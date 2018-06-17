3bb3bb5cd6dc04589930ee58c2f8b687d8e2111a
Jessele Duran <jesseleadt@gmail.com>
2018-06-16 21:06:30 -0400

    home page

--- src/main/resources/static/js/client.js
+++ src/main/resources/static/js/client.js
@@ -1,24 +1,36 @@
+$(document).ready(function(){
+    $('#body').load('views/home.html');
+
+});
+
 var userName = 'user' + Math.floor((Math.random() * 1000) + 1);
 var socket = io('http://localhost:9092/chat');
 socket.connect();
 
+//when a user connects
 socket.on('connect', function () {
     output('<span class="connect-msg">Client has connected to the server!</span>');
 });
 
+//send chat message
 socket.on('chat', function (data) {
     console.log('Received message', data);
     output('<span class="username-msg">' + data.userName + ':</span> ' + data.message);
 });
 
+//disconnect from network
 socket.on('disconnect', function () {
     output('<span class="disconnect-msg">The client has disconnected!</span>');
 });
 
+//quantity of connected
 socket.on('cantidadConectados', function (data) {
     console.log('Received conectados', data.conectados);
+    var conectados = data.conectados;
+    $(".displayConectados").html(conectados);
 });
 
+//divide .html to different users
 socket.on('htmlType', function (data) {
     document.getElementById("test").textContent = data;
 });
@@ -46,3 +58,7 @@ $(document).keydown(function (e) {
         $('#send').click();
     }
 });
+
+
+
+
